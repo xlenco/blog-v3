ee682c38b257ea736c112dcbc4f8600619da2226
xlenco <1043865083@qq.com>
2025-04-26 11:48:58 +0800

    fix: Connectivity.vue

--- app/components/widget/Connectivity.vue
+++ app/components/widget/Connectivity.vue
@@ -2,26 +2,18 @@
 const appConfig = useAppConfig()
 </script>
 
-<!-- <template>
+<template>
     <h3 class="widget-title">
         可访问性
     </h3>
-    <div
-        class="widget-card seasonal"
-        :style="{
-            '--seasonal-bg': `url(${appConfig.seasonal.widgetBackground})`,
-            '--seasonal-emoji': appConfig.seasonal.emoji,
-        }"
-    >
-     -->
+    <div class="widget-card seasonal" :style="{
+        '--seasonal-bg': `url(${appConfig.seasonal.widgetBackground})`,
+        '--seasonal-emoji': appConfig.seasonal.emoji,
+    }">
+
         <!-- TODO: 优化技术架构展示 -->
-<!--         <p>域名启用 DNSSEC 和 HSTS，续费至2030年8月，托管于 Vercel；博客由 Nuxt Content 驱动，使用去图图床。</p>
-        <p>
-            获得省级、国家级认证如下：<br>
-            江苏:<span class="seasonal-emoji">跳转反诈宣传</span><br>
-            重庆:<span class="seasonal-emoji">DNS污染</span><br>
-            移动:<span class="seasonal-emoji">多省阻断解析</span>
-        </p>
+        <p>托管于 Vercel；博客由 Nuxt Content 驱动。</p>
+
     </div>
 </template> -->
 
@@ -42,7 +34,8 @@ const appConfig = useAppConfig()
     }
 }
 
-.seasonal-emoji::before, .seasonal-emoji::after {
+.seasonal-emoji::before,
+.seasonal-emoji::after {
     // 文明用语😋
     content: var(--seasonal-emoji, "\1F595");
 }
